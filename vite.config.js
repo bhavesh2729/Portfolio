690ca5dcc9f64963bbf0cd4815f4793ceff6623b
Bhavesh Gaurkar <bhaveshgaurkar347@gmail.com>
2026-09-19 16:54:23 +0530

    Fix Render host

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   preview: {
-    allowedHosts: ['portfolio-xhxz.onrender.com'],
+    allowedHosts: ['portfolio-flec.onrender.com'],
   },
 })
